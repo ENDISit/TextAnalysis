a9ea3f722cca73ffcb2c485a1abfeb8cf9233506
ENDISit <mr.ilia08@mail.ru>
2024-06-07 00:18:01 +0300

    Добавьте файлы проекта.

---
+++ TextAnalysis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class TextAnalyzer
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите текст для анализа:");
+        string text = Console.ReadLine();
+
+        int wordCount = text.Split(new[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        int sentenceCount = text.Split(new[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        int paragraphCount = text.Split(new[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+        Console.WriteLine($"Количество слов: {wordCount}");
+        Console.WriteLine($"Количество предложений: {sentenceCount}");
+        Console.WriteLine($"Количество абзацев: {paragraphCount}");
+
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadLine();
+    }
+}
